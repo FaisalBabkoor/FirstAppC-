eb7c6b0e4c4bdfcf297cfbdcea74546437f7da7a
Faisal <faisal-f92@hotmail.com>
2020-08-22 02:44:45 +0300

    Initial Commit

---
+++ FirstAppC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FirstAppC++
+//
+//  Created by Faisal Babkoor on 8/22/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
